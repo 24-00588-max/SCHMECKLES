a532a332267fff03a3d9e9ded5afa84e10c5f1a2
24-00588-max <24-00588@csa.edu.ph>
2026-04-28 05:40:52 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,12 @@
+const USERS = [
+  { id: "1001", name: "Alex", pin: "111", lambda: 0 },
+  { id: "1002", name: "William", pin: "121", lambda: 0 },
+  { id: "1003", name: "Noah", pin: "131", lambda: 0 },
+  { id: "1004", name: "Ariel", pin: "141", lambda: 0 },
+  { id: "1005", name: "Leon", pin: "151", lambda: 0 },
+  { id: "1006", name: "Pablo", pin: "161", lambda: 0 },
+  { id: "1007", name: "Miya", pin: "171", lambda: 0 },
+  { id: "1008", name: "Lexie", pin: "181", lambda: 0 },
+  { id: "1009", name: "Sophia", pin: "191", lambda: 0 },
+  { id: "1010", name: "Chloe", pin: "201", lambda: 0 }
+];
